28e7b2f32f355721b0b95e902180d78bdac21f37
galt680 <galt680@gmail.com>
2016-09-25 15:06:26 -0400

    updates

--- live_tech.py
+++ live_tech.py
@@ -63,7 +63,8 @@ class Tech(object):
 		elif time == "daily":
 	
 			#get the daily data 
-			url = "https://www.google.com/finance/getprices?i=86400&p=250d&f=c,o,h,l&df=cpct&q=%s"%(name.upper())
+			url = "https://www.google.com/finance/getprices?i=86400&p=1Y&f=c,o,h,l&df=cpct&q=%s"%(name.upper())
+			# url = "https://www.google.com/finance/getprices?i=86400&p=250d&f=c,o,h,l&df=cpct&q=%s"%(name.upper())
 			page = requests.get(url).text
 
 			prices = re.findall(r'''\n(\d+\.\d+,\d+\.\d+,\d+\.\d+,\d+\.\d+)''',page)
@@ -94,42 +95,47 @@ class Tech(object):
 				self.low.append(most_recent_low)	
 		else:
 			print "what went wrong"
+		self.high = np.array(self.high)[-225:]
+		self.low = np.array(self.low)[-225:]
+		self.close = np.array(self.close)[-225:]
+		# self.open = np.array(self.open)
+
 	def prints(self):
 		print "close"
-		print self.close[-10:]
+		print len(self.close)
 		print "open"
-		print self.open[-10:]
+		print len(self.open)
 		print "high"
-		print self.high[-10:]
+		print len(self.high)
 		print "low"
-		print self.low[-10:]
-	# def rsi(self):
-		# real = ta.RSI(((self.close)), timeperiod=14)
-		# return round(real[-1],2)
+		print len(self.low)
+	def rsi(self):
+		real = ta.RSI((self.close), timeperiod=14)
+		return round(real[-1],2)
 
-	# def slow_stoch(self):
-		# slowk, slowd = ta.STOCH(self.high, self.low, self.close, fastk_period=10, slowk_period=10, slowk_matype=0, slowd_period=3, slowd_matype=0)
-		# slowk,slowd = slowk[-1],slowd[-1]
-		# return round(slowk,2),round(slowd,2)
-	# def overbought(self):
-		# slowk,slowd = self.slow_stoch()
-		# if ((slowk > 70) and (slowd	 > 70)) and (self.rsi() > 60):
-			# return True #"%s is overbought with RSI at %s and the stochastics at %s,%s"%(self.name.upper(),round(self.rsi(),2),round(slowk,2),round(slowd,2))
-		# else:
-			# return False
-	# def oversold(self):
-		# slowk,slowd = self.slow_stoch()
-		# if ((slowk < 30) and (slowd	 < 30)) and (self.rsi() < 35):
-			# return True
-		# else:
-			# return False
+	def slow_stoch(self):
+		slowk, slowd = ta.STOCH(self.high, self.low, self.close, fastk_period=10, slowk_period=10, slowk_matype=0, slowd_period=3, slowd_matype=0)
+		slowk,slowd = slowk[-1],slowd[-1]
+		return round(slowk,2),round(slowd,2)
+	def overbought(self):
+		slowk,slowd = self.slow_stoch()
+		if ((slowk > 70) and (slowd	 > 70)) and (self.rsi() > 60):
+			return True #"%s is overbought with RSI at %s and the stochastics at %s,%s"%(self.name.upper(),round(self.rsi(),2),round(slowk,2),round(slowd,2))
+		else:
+			return False
+	def oversold(self):
+		slowk,slowd = self.slow_stoch()
+		if ((slowk < 30) and (slowd	 < 30)) and (self.rsi() < 35):
+			return True
+		else:
+			return False
 
-	# def signals(self):
-		# if self.oversold() == True:
-			# return 1
-		# elif self.overbought() == True:
-			# return -1
-		# else:
-			# return 0
-a = Tech('goog',time = 'daily')
-a.prints()
+	def signals(self):
+		if self.oversold() == True:
+			return 1
+		elif self.overbought() == True:
+			return -1
+		else:
+			return 0
+# a = Tech('goog',time = 'daily')
+# a.prints()
