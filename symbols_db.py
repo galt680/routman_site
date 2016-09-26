e0882b0dd796f4cb49e0efb922794dd3f02e4e46
galt680 <galt680@gmail.com>
2016-09-25 21:18:33 -0400

    I hope these updates work

--- symbols_db.py
+++ symbols_db.py
@@ -1,43 +1,41 @@
 import sqlite3 as lite
 from tech import Tech
 import timeit
-start = timeit.default_timer()
-aapl = Tech('goog')
-# print aapl.prints()
-# print aapl.prints()
-# symbols = [
-		# 'AAL','AAPL','ABT','ABBV','ABX','ACAD','ADBE','ADP','AET','AGO','ALL','AMAT','AMD','AMGN','APA','AXP','BA','BAC','BAX','BCLYF',
-		# 'BHI','BMY','BP','C','CAR','CBI','CELG','CMCSA','CNI','COP', 'CREE','CSCO','CREE','CTL','CLX','CTSH','CVS','CVX','DFS','DIS',
-		# 'DO','DTV','DVN','ESRX','EXAS','F','FB','FCX','FITB','FLR','GE','GILD','GLW','GS','L_GSK','HAL','HP','HPQ','IBM','INTC','INTU',
-		# 'JNJ','JPM','KEY','NYSE_KKR','KR','KRFT','KSU','L','LLY','LNKD','LOW','MCK','MET','MDT','MMM','MON','MRK','MSFT','MUR','MYL','NBL',
-		# 'NE','NFX','NTRS','NVSEF','ORCL','OXY','P','PE','PEP','PFE','PG','PLUG','PM','PNC','PSX','PXD','QCOM','RRC','SCHW','STLD','STT',
-		# 'T','TA_TEVA','TRV','TSO','TWTR','TXN','TYC','UBS','UNGS','UNH','URI','USB','UTX','VRX','VZ','WFC','X','YHOO','XOM','GOOG','JPM',
-		# 'PCLN',
-	# ]
+	
 
-# dic = {}
-# for i in symbols:
-	# try:
-		# tech = Tech(i,time = "daily")
-		# dic[i] = {'signal':tech.signals(),
-				  # "RSI":tech.rsi(),
-				  # "Stochastic":tech.slow_stoch()}
-		# print "%s successful"%i
-	# except Exception as e:
-		# print e
-		# print "%s unsuccessful"%i
+symbols = [
+		'AAL','AAPL','ABT','ABBV','ABX','ACAD','ADBE','ADP','AET','AGO','ALL','AMAT','AMD','AMGN','APA','AXP','BA','BAC','BAX','BCLYF',
+		'BHI','BMY','BP','C','CAR','CBI','CELG','CMCSA','CNI','COP', 'CREE','CSCO','CREE','CTL','CLX','CTSH','CVS','CVX','DFS','DIS',
+		'DO','DTV','DVN','ESRX','EXAS','F','FB','FCX','FITB','FLR','GE','GILD','GLW','GS','L_GSK','HAL','HP','HPQ','IBM','INTC','INTU',
+		'JNJ','JPM','KEY','NYSE_KKR','KR','KRFT','KSU','L','LLY','LNKD','LOW','MCK','MET','MDT','MMM','MON','MRK','MSFT','MUR','MYL','NBL',
+		'NE','NFX','NTRS','NVSEF','ORCL','OXY','P','PE','PEP','PFE','PG','PLUG','PM','PNC','PSX','PXD','QCOM','RRC','SCHW','STLD','STT',
+		'T','TA_TEVA','TRV','TSO','TWTR','TXN','TYC','UBS','UNGS','UNH','URI','USB','UTX','VRX','VZ','WFC','X','YHOO','XOM','GOOG','JPM',
+		'PCLN',
+	]
 
-# con = lite.connect('/home/yaschaffel/mysite/ALERT_DATA.db')
-# con = lite.connect('ALERT_DATA.db')
-# cur = con.cursor()
-# cur.execute("DROP TABLE IF EXISTS ALERT_DATA")
-# cur.execute("CREATE TABLE ALERT_DATA(NAME TEXT,SIGNAL INT,RSI INT,SLOWK INT,SLOWD INT)")
-# for i in dic:
-	# cur.execute("INSERT INTO ALERT_DATA(NAME,SIGNAL,RSI,SLOWK,SLOWD) VALUES (?,?,?,?,?)",(i,dic[i]['signal'],dic[i]['RSI'],dic[i]['Stochastic'][0],dic[i]['Stochastic'][0]))
-# con.commit()
-# stop = timeit.default_timer()
-# try:
-    # print "Task took %s minutes"%(int(stop - start)/60)
-# except:
-    # pass
+dic = {}
+for i in symbols:
+	try:
+		tech = Tech(i,time = "daily")
+		dic[i] = {'signal':tech.signals(),
+				  "RSI":tech.rsi(),
+				  "Stochastic":tech.slow_stoch()}
+		print "%s successful"%i
+	except Exception as e:
+		print e
+		print "%s unsuccessful"%i
+
+con = lite.connect('/home/yaschaffel/mysite/ALERT_DATA.db')
+con = lite.connect('ALERT_DATA.db')
+cur = con.cursor()
+cur.execute("DROP TABLE IF EXISTS ALERT_DATA")
+cur.execute("CREATE TABLE ALERT_DATA(NAME TEXT,SIGNAL INT,RSI INT,SLOWK INT,SLOWD INT)")
+for i in dic:
+	cur.execute("INSERT INTO ALERT_DATA(NAME,SIGNAL,RSI,SLOWK,SLOWD) VALUES (?,?,?,?,?)",(i,dic[i]['signal'],dic[i]['RSI'],dic[i]['Stochastic'][0],dic[i]['Stochastic'][0]))
+con.commit()
+stop = timeit.default_timer()
+try:
+    print "Task took %s minutes"%(int(stop - start)/60)
+except:
+    pass
 
--- tech.py
+++ tech.py
@@ -6,54 +6,85 @@ from live_tech import Tech
 import requests
 import re
 
-
+#create class 
 class Tech(object):
 
-	def __init__(self,name):
+	def __init__(self,name,time = "daily"):
 		self.name = name
-		try:
-			self.data = q.get('YAHOO/%s'%name.upper(),authtoken =  auth)
-			
-		except:
+		if time == "daily":
 			try:
-				self.data = q.get('GOOG/%s'%name.upper(),authtoken =  auth)
-			except:
-				pass
+				self.data = q.get('YAHOO/%s'%name.upper(),authtoken =  auth)
+				self.data = self.data.round(decimals = 3)
 				
-		url = "https://www.google.com/finance/getprices?i=60&p=1d&f=c,o,h,l&df=cpct&q=%s"%(name.upper())
-		page = requests.get(url).text
-		prices = re.findall(r'''\n(\d+\.\d+,\d+\.\d+,\d+\.\d+,\d+\.\d+)''',page)
-		most_recent_close = float(prices[-1].split(',')[0])
+			except:
+				try:
+					self.data = q.get('GOOG/%s'%name.upper(),authtoken =  auth)
+				except:
+					pass
+					
+			url = "https://www.google.com/finance/getprices?i=60&p=1d&f=c,o,h,l&df=cpct&q=%s"%(name.upper())
+			page = requests.get(url).text
+			prices = re.findall(r'''\n(\d+\.\d+,\d+\.\d+,\d+\.\d+,\d+\.\d+)''',page)
+			most_recent_close = float(prices[-1].split(',')[0])
+			
+			url = "https://www.google.com/finance/getprices?i=86400&p=250d&f=c,o,h,l&df=cpct&q=%s"%(name.upper())
+			page = requests.get(url).text
+			prices = re.findall(r'''\n(\d+\.\d+,\d+\.\d+,\d+\.\d+,\d+\.\d+)''',page)
+			most_recent_high = float(prices[-1].split(',')[1])
+			most_recent_low = float(prices[-1].split(',')[2])
+			most_recent_open = float(prices[-1].split(',')[3])
+			
+			if round(self.data['Close'][-1],2) == round(most_recent_close,2):
+				self.high = np.array(list(self.data['High']))
+				self.low = np.array(list(self.data['Low']))
+				self.close = np.array(list(self.data['Close']))
+				
+			else:
+				self.high = np.array(list(self.data['High'])+ [most_recent_high])
+				self.low = np.array(list(self.data['Low']) + [most_recent_low])
+				self.close = np.array(list(self.data['Close'])+ [most_recent_close])
+		elif time == "hourly":
 		
-		url = "https://www.google.com/finance/getprices?i=86400&p=250d&f=c,o,h,l&df=cpct&q=%s"%(name.upper())
-		page = requests.get(url).text
-		prices = re.findall(r'''\n(\d+\.\d+,\d+\.\d+,\d+\.\d+,\d+\.\d+)''',page)
-		most_recent_high = float(prices[-1].split(',')[1])
-		most_recent_low = float(prices[-1].split(',')[2])
-		most_recent_open = float(prices[-1].split(',')[3])
-		
-		# print most_recent_close
-		
-		# self.high = np.array(list(self.data['High'])+ [most_recent_high]
-		# self.low = np.array(list(self.data['Low']).append(most_recent_low))
-		# self.close = np.array(list(self.data['Close']).append(most_recent_close))
-		a = list(self.data['Close'])+ [most_recent_close]
-		
-		print a
-		# print most_recent_close
-		# print round(a[-1],2)
-		# self.opens = np.array(list(self.data['Open']).append(most_recent_open))
-		# print self.close
+			#first get the minute data to add to the end of the most recent hourly data to make sure it's up to date
+			url = "https://www.google.com/finance/getprices?i=60&p=1d&f=c,o,h,l&df=cpct&q=%s"%(name.upper())
+			page = requests.get(url).text
+			prices = re.findall(r'''\n(\d+\.\d+,\d+\.\d+,\d+\.\d+,\d+\.\d+)''',page)
+			most_recent_close = float(prices[-1].split(',')[0])
+			most_recent_high = float(prices[-1].split(',')[1])
+			most_recent_low = float(prices[-1].split(',')[2])
+			most_recent_open = float(prices[-1].split(',')[3])
+			
+			#get the hourly data 
+			url = "https://www.google.com/finance/getprices?i=3600&p=10d&f=c,o,h,l&df=cpct&q=%s"%(name.upper())
+			page = requests.get(url).text
 
-	# def prints(self):
-		# print "close"
-		# print (self.close)
-		# print "open"
-		# print (self.opens)[-10:]
-		# print "high"
-		# print (self.high)[-10:]
-		# print "low"
-		# print (self.low)[-10:]
+			prices = re.findall(r'''\n(\d+\.\d+,\d+\.\d+,\d+\.\d+,\d+\.\d+)''',page)
+			self.open = []
+			self.high = []
+			self.low = []
+			self.close = []
+			for i in prices:
+				self.close.append(float(i.split(",")[0]))
+				self.high.append(float(i.split(',')[1]))
+				self.low.append(float(i.split(',')[2]))
+				self.open.append(float(i.split(',')[3]))
+				
+			#check if the most recent minute data piece equals the most recent houry if does pass if not add most recent to hourly 
+			if most_recent_close == self.close[-1]:
+				pass
+			else:
+				self.close.append(most_recent_close)
+				self.open.append(most_recent_open)
+				self.high.append(most_recent_high)
+				self.low.append(most_recent_low)
+		
+			#convert all lists to numpy arrays to comply with talib library
+			self.open = np.array(self.open)
+			self.high = np.array(self.high)
+			self.low = np.array(self.low)
+			self.close = np.array(self.close)
+
+
 
 	def rsi(self):
 		real = ta.RSI(((self.close)), timeperiod=14)
