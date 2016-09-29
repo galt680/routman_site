19c69d2bad192acb236da56ad04dd4f23e226af8
galt680 <galt680@gmail.com>
2016-09-29 15:22:48 -0400

    removed password from ivol

--- ivol.py
+++ ivol.py
@@ -4,6 +4,8 @@ import re
 import time
 import timeit
 import os
+import sqlite3 as lite
+from passwords import ivol
 
 
 start = timeit.default_timer()
@@ -13,7 +15,7 @@ if os.path.exists('C:\\Users\\Yasch'):
 	caps = DesiredCapabilities.FIREFOX
 	caps["marionette"] = True
 	caps["binary"] = "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe"
-	browser = webdriver.Firefox(capabilities=caps)
+	driver = webdriver.Firefox(capabilities=caps)
 	con = lite.connect('IMP_VOL_TABLE.db')
 else:
 	con = lite.connect('/home/yaschaffel/mysite/IMP_VOL_TABLE.db')
@@ -24,45 +26,80 @@ else:
 
 
 
-browser.get('https://www.ivolatility.com/login.j')
-username = browser.find_element_by_name('username')
-username.send_keys('galt680')
-password = browser.find_element_by_name('password')
-password.send_keys('85057047')
-browser.find_element_by_css_selector('.btn.btn-custom.btn-login').click()
+driver.get('https://www.ivolatility.com/login.j')
+username = driver.find_element_by_name('username')
+username.send_keys('')
+password = driver.find_element_by_name('password')
+password.send_keys(ivol)
+driver.find_element_by_css_selector('.btn.btn-custom.btn-login').click()
 time.sleep(3)
 
 
-con = lite.connect('IMP_VOL_TABLE.db')
-cur = con.cursor()
 
-cur.execute('update IMP_VOL set YESTERDAY_VALUE = TODAY_VALUE')
-for i in sp500:
+driver.get('http://www.ivolatility.com/options.j?ticker=afl&R=0&top_lookup__is__sent=1')
+aapl_current = float(driver.find_element_by_xpath("/html/body/div/div[3]/div[2]/table[1]/tbody/tr[3]/td[1]/table[2]/tbody/tr[9]/td[2]/font").text.strip('%'))
+driver.get('http://www.ivolatility.com/options.j?ticker=afl&R=0&top_lookup__is__sent=1')
+afl_current = float(driver.find_element_by_xpath("/html/body/div/div[3]/div[2]/table[1]/tbody/tr[3]/td[1]/table[2]/tbody/tr[9]/td[2]/font").text.strip('%'))
+if (aapl_current != 26.76) and (afl_current != 15.39):
+    print datetime.datetime.now()
+else:
+    print "Nothing Changed"
+	
+	
+	
+	
+	
+	
+print "logging out"
+try:
+    driver.find_element_by_xpath('/html/body/nav/div/div[1]/span/span[1]/a/span').click()
+    time.sleep(2)
+    driver.find_element_by_xpath('/html/body/nav/div/div[1]/span/span[1]/div/ul/li[6]/a/i').click()
+    print "wait 2"
+except:
     try:
-        url = "http://www.ivolatility.com/options.j?ticker=%s&R=0&top_lookup__is__sent=1"%i['symbol']
-        browser.get(url)
-            try:
-        current = float(browser.find_element_by_xpath("/html/body/div/div[3]/div[2]/table[1]/tbody/tr[3]/td[1]/table[2]/tbody/tr[9]/td[2]/font").text.strip('%'))
-            except:
-                print i['symbol']
-                pass
-        cur.execute("""update IMP_VOL set TODAY_VALUE = %s where name = '%s'"""%(current,i['symbol']))
-        time.sleep(random.uniform(0,1.5))
+        driver.find_element_by_xpath('/html/body/nav/div/div[1]/span/span[1]/a/span').click()
+        time.sleep(1)
+        driver.find_element_by_xpath('/html/body/nav/div/div[1]/span/span[1]/div/ul/li[6]/a/i').click()
+        print "wait 1"
     except:
-        pass
-con.commit()
-con.close()
-browser.find_element_by_xpath('/html/body/nav/div/div[1]/span/span[1]/a/span').click()
-time.sleep(3)
-browser.find_element_by_xpath('/html/body/nav/div/div[1]/span/span[1]/div/ul/li[6]/a/i').click()
+        driver.find_element_by_xpath('/html/body/nav/div/div[1]/span/span[1]/a/span').click()
 
-stop = timeit.default_timer()
-try:
-    print "Task took %s minutes"%(int(stop - start)/60)
-except:
-    pass
-driver.quit()
-try:
-	display.stop()
-except:
-	pass
+        driver.find_element_by_xpath('/html/body/nav/div/div[1]/span/span[1]/div/ul/li[6]/a/i').click()
+        print "no wait"
+print "logged out"
+
+
+
+# cur = con.cursor()
+
+# cur.execute('update IMP_VOL set YESTERDAY_VALUE = TODAY_VALUE')
+# for i in sp500:
+    # try:
+        # url = "http://www.ivolatility.com/options.j?ticker=%s&R=0&top_lookup__is__sent=1"%i['symbol']
+        # driver.get(url)
+            # try:
+        # current = float(driver.find_element_by_xpath("/html/body/div/div[3]/div[2]/table[1]/tbody/tr[3]/td[1]/table[2]/tbody/tr[9]/td[2]/font").text.strip('%'))
+            # except:
+                # print i['symbol']
+                # pass
+        # cur.execute("""update IMP_VOL set TODAY_VALUE = %s where name = '%s'"""%(current,i['symbol']))
+        # time.sleep(random.uniform(0,1.5))
+    # except:
+        # pass
+# con.commit()
+# con.close()
+# driver.find_element_by_xpath('/html/body/nav/div/div[1]/span/span[1]/a/span').click()
+# time.sleep(3)
+# driver.find_element_by_xpath('/html/body/nav/div/div[1]/span/span[1]/div/ul/li[6]/a/i').click()
+
+# stop = timeit.default_timer()
+# try:
+    # print "Task took %s minutes"%(int(stop - start)/60)
+# except:
+    # pass
+# driver.quit()
+# try:
+	# display.stop()
+# except:
+	# pass
