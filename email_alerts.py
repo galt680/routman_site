7b2b12b67b3d613109664f81b261f7e21ed40dfa
galt680 <galt680@gmail.com>
2016-10-07 12:01:54 -0400

    added SPY email to email_alerts

--- email_alerts.py
+++ email_alerts.py
@@ -6,7 +6,7 @@ import datetime
 from passwords import pswd
 
 
-def send_alert():
+def send_watchlist(test = False):
 	today = datetime.date.today()
 	alert_overbought = []
 	alert_oversold = []
@@ -43,7 +43,7 @@ def send_alert():
 			else:
 				pass
 
-	message = """Good evening,\n(This is an automated message)\n\nThese are the stocks that are currently overbought from your watchlist:\n"""
+	message = """Good evening,\n(This is an automated message)\n\nThese are the stocks that are currently overbought and oversold from your watchlist:\n"""
 	for i in alert_overbought:
 		message +='\n %s'%i
 	message +="""\n\nThese are the stocks that are currently oversold from your watchlist:\n"""
@@ -64,19 +64,99 @@ def send_alert():
 	msg = MIMEMultipart()
 	msg['From'] = fromaddr
 	msg['To'] = toaddr
-	msg['Subject'] = "Alerts for %s"%today.strftime("%m-%d-%Y")
+	msg['Subject'] = "Watchlist Alerts for %s"%today.strftime("%m-%d-%Y")
+
+
+	body = message
+	print body
+	if test == True:
+		pass
+	else:
+		msg.attach(MIMEText(body, 'plain'))
+
+
+		server = smtplib.SMTP('smtp.gmail.com', 587)
+		server.starttls()
+		server.login("routmanapp@gmail.com", pswd)
+		text = msg.as_string()
+		server.sendmail(fromaddr, toaddr, text)
+		server.quit()
+		print "Watchlist Email sent!"
+
+def send_spy(test = False):
+	today = datetime.date.today()
+	alert_overbought = []
+	alert_oversold = []
+	rsi_overbought = []
+	rsi_oversold = []
+	try:
+		print 1
+		con = lite.connect('/home/yaschaffel/mysite/ALERT_DATA.db')
+		print 1
+	except:
+		print 2
+		con = lite.connect('ALERT_DATA.db')
+		print 2
+	cur = con.cursor()
+	with con:
+		cur = con.cursor()
+		cur.execute("SELECT * FROM SPY_DATA WHERE SIGNAL != 0 ORDER BY Name Asc")
+		for name,signal,rsi,slowk,slowd in cur.fetchall():
+			if signal == -1:
+				alert_overbought.append("%s is overbought with RSI at %s and the stochastics at %s and at %s"%(name,rsi,slowk,slowd))
+			elif signal == 1:
+				alert_oversold.append("%s is oversold with RSI at %s and the stochastics at %s and at %s"%(name,rsi,slowk,slowd))
+			else:
+				pass
+				
+		cur.execute("SELECT * FROM SPY_DATA WHERE RSI NOT BETWEEN 35 AND 65 ORDER BY RSI DESC")
+		for name,signal,rsi,slowk,slowd in cur.fetchall():
+			if rsi > 65:
+				rsi_overbought.append("%s is overbought with RSI at %s"%(name,rsi))
+			elif rsi < 35:
+				rsi_oversold.append("%s is oversold with RSI at %s"%(name,rsi))	
+			else:
+				pass
+
+	message = """Good evening,\n(This is an automated message)\n\nThese are the stocks that are currently overbought and oversold from the S&P500:\n"""
+	for i in alert_overbought:
+		message +='\n %s'%i
+	message +="""\n\nThese are the stocks that are currently oversold from the S&P500:\n"""
+	for i in alert_oversold:
+		message += '\n %s'%i
+	message +="""\n\nThese are the stocks that are currently overbought based only on RSI from the S&P500:\n"""
+	for i in rsi_overbought:
+		message += '\n %s'%i
+	message +="""\n\nThese are the stocks that are currently oversold based only on RSI from the S&P500:\n"""
+	for i in rsi_oversold:
+		message += '\n %s'%i
+	message += "\n\n If you have any adjustments you'd like to make to the alert settings or if you'd like to add symbols to the watchlist please let me know.\nYisroel Schaffel"
+
+
+
+	fromaddr = "routmanapp@gmail.com"
+	toaddr = "aronroutman@sbcglobal.net" ##
+	msg = MIMEMultipart()
+	msg['From'] = fromaddr
+	msg['To'] = toaddr
+	msg['Subject'] = "SPY ALERTS for %s"%today.strftime("%m-%d-%Y")
 
 
 	body = message
 	print body
 	msg.attach(MIMEText(body, 'plain'))
 
-
-	server = smtplib.SMTP('smtp.gmail.com', 587)
-	server.starttls()
-	server.login("routmanapp@gmail.com", pswd)
-	text = msg.as_string()
-	server.sendmail(fromaddr, toaddr, text)
-	server.quit()
-	print "Email sent!"
-send_alert()
+	if test == True:
+		pass
+	else:
+		server = smtplib.SMTP('smtp.gmail.com', 587)
+		server.starttls()
+		server.login("routmanapp@gmail.com", pswd)
+		text = msg.as_string()
+		server.sendmail(fromaddr, toaddr, text)
+		server.quit()
+		print "SPY Email sent!"
+	
+	
+send_watchlist()
+send_spy()
