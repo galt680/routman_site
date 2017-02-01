ffec101c0f8a5ccf8cab6d7395aa2bfda61bc0d9
galt680 <galt680@gmail.com>
2017-02-01 14:46:14 -0500

    completed send_alert funciton; possible adjust watchlist_name variable

--- email_alerts.py
+++ email_alerts.py
@@ -8,22 +8,85 @@ try:
 except:
     from email.MIMEMultipart import MIMEMultipart
     from email.MIMEText import MIMEText
-def send_alert(db_name, table_name, test = False):
-	today = datetime.date.today()
-	rsi_oversold     = []
-	rsi_overbought   = []
-	alert_oversold   = []
-	alert_overbought = []
-	
-	con = connect_to_db(db_name)
-	with con:
-		cur = con.cursor()
-		cur.execute("SELECT * FROM %s WHERE SIGNAL != 0 ORDER BY NAME ASC"%table_name)
-		for name,signal,rsi,slowk,slowd in cur.fetchall():
-			if signal == -1:
-				alert_overbought.append("%s is overbought with RSI at %s and the stochastics at %s and %s"%(name,rsi,slowk,slowd))
-			elif signal == 1:
-				alert_oversold.append("%s is oversold with RSI at %s and the stochastics at %s and at %s"%(name,rsi,slowk,slowd)) 
+def send_alert(db_name = None, table_name = None, watchlist_name = None,  test = False):
+    today = datetime.date.today()
+    rsi_oversold     = []
+    rsi_overbought   = []
+    alert_oversold   = []
+    alert_overbought = []
+    
+    con = connect_to_db(db_name)
+    with con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM %s WHERE SIGNAL != 0 ORDER BY NAME ASC"%table_name)
+        for name,signal,rsi,slowk,slowd in cur.fetchall():
+            if signal == -1:
+                alert_overbought.append("%s is overbought with RSI at %s and the stochastics at %s and %s"%(name,rsi,slowk,slowd))
+            elif signal == 1:
+                alert_oversold.append("%s is oversold with RSI at %s and the stochastics at %s and at %s"%(name,rsi,slowk,slowd))
+            else:
+                pass
+    with con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM %s WHERE SIGNAL != 0 ORDER BY Name Asc"%table_name)
+        for name,signal,rsi,slowk,slowd in cur.fetchall():
+            if signal == -1:
+                alert_overbought.append("%s is overbought with RSI at %s and the stochastics at %s and at %s"%(name,rsi,slowk,slowd))
+            elif signal == 1:
+                alert_oversold.append("%s is oversold with RSI at %s and the stochastics at %s and at %s"%(name,rsi,slowk,slowd))
+            else:
+                pass
+
+        cur.execute("SELECT * FROM %s WHERE RSI NOT BETWEEN 35 AND 65 ORDER BY RSI DESC"%table_name)
+        for name,signal,rsi,slowk,slowd in cur.fetchall():
+            if rsi > 65:
+                rsi_overbought.append("%s is overbought with RSI at %s"%(name,rsi))
+            elif rsi < 35:
+                rsi_oversold.append("%s is oversold with RSI at %s"%(name,rsi))
+            else:
+                pass
+
+    message = """Good evening,\n(This is an automated message)\n\nThese are the stocks that are currently overbought and oversold from %s:\n"""%watchlist_name
+    for i in alert_overbought:
+        message +='\n %s'%i
+    message +="""\n\nThese are the stocks that are currently oversold from %s:\n"""%watchlist_name
+    for i in alert_oversold:
+        message += '\n %s'%i
+    message +="""\n\nThese are the stocks that are currently overbought based only on RSI from %s:\n"""%watchlist_name
+    for i in rsi_overbought:
+        message += '\n %s'%i
+    message +="""\n\nThese are the stocks that are currently oversold based only on RSI from %s:\n"""%watchlist_name
+    for i in rsi_oversold:
+        message += '\n %s'%i
+    message += "\n\n If you have any adjustments you'd like to make to the alert settings or if you'd like to add symbols to the watchlist please let me know.\nYisroel Schaffel"
+
+
+
+    fromaddr = "routmanapp@gmail.com"
+    toaddr = "aronroutman@sbcglobal.net" ##
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = "Watchlist Alerts for %s"%today.strftime("%m-%d-%Y")
+
+
+    body = message
+    print (body)
+    if test == True:
+        pass
+    else:
+        msg.attach(MIMEText(body, 'plain'))
+
+
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login("routmanapp@gmail.com", pswd)
+        text = msg.as_string()
+        server.sendmail(fromaddr, toaddr, text)
+        server.quit()
+        print ("Watchlist Email sent!")
+
+        
 def send_watchlist(test = False):
     today = datetime.date.today()
     alert_overbought = []
@@ -382,8 +445,10 @@ def send_spy_weekly(test = False):
         server.sendmail(fromaddr, toaddr, text)
         server.quit()
         print ("SPY Email sent!")
+
+send_alert(db_name = "ALERT_DATA",table_name = 'ALERT_DATA',watchlist_name= "your watchlist")       
 # send_spy_weekly(test = True)
-send_watchlist(test = True)
+# send_watchlist(test = True)
 # send_spy(test = True)
 # send_watchlist_weekly(test = True)
 # send_watchlist_2017(test = True)
